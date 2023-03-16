6c353cd8b74add6df6aa2c5ffebfc1e1817581cf
Kamran030 <kamranmammadov030@gmail.com>
2023-03-16 18:02:05 +0400

    Route node js

--- server.js
+++ server.js
@@ -1,29 +1,6 @@
-// !Request different pages
-
-var http = require("http");
-var fs = require("fs");
-
-var server = http.createServer((request, response) => {
-  if (request.url == "/") {
-    fs.readFile("index.html", (error, html) => {
-      response.writeHead(200, { "Content-Type": "text/html" });
-      response.write(html);
-      response.end();
-    });
-  } else if (request.url == "/blogs") {
-    fs.readFile("blogs.html", (error, html) => {
-      response.writeHead(200, { "Content-Type": "text/html" });
-      response.write(html);
-      response.end();
-    });
-  } else {
-    fs.readFile("error.html", (error, html) => {
-      response.writeHead(404, { "Content-Type": "text/html" });
-      response.write(html);
-      response.end();
-    });
-  }
-});
+const http = require("http");
+const routes = require("./routes")
+var server = http.createServer(routes);
 
 server.listen(3000);
 console.log("REQUEST");
